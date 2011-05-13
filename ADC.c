3081a9ec1d6de6b054ece7c3f880ff465101dbda
aka.hector <aka.hector@49ee0174-1f45-f5d8-f732-777557fac2e5>
2011-05-13 22:46:00 +0000

    added configuration of ANSEL registers to disable digital buffers

--- ADC.c
+++ ADC.c
@@ -18,24 +18,22 @@ int Kp,Ki,Kd,new_error,old_error,P_err,I_err,D_err;
 #include "rosefloat.h"
 
 
-int PID( void )
+int PID()
 {
-	int dp = 0;
-	int cp = 0;
 
 	old_error = new_error;
-	new_error = dp-cp;
+	new_error = desired_position-current_position;
 	P_err = new_error;
 	I_err += old_error;
 	D_err = new_error - old_error;
 	
-	return (1*P_err + 1*I_err + 1*D_err); //Gains set to 1 for testing
+	return (1*P_err + 0*I_err + 0*D_err); //Gains set to 1 for testing
 
 }
 int Get_ADC( int i )
 {
-	//ADCON0 = 0x03; //Selects ADChannel "i" and turns on ADC
-	ADCON0 = (i << 4) + 3;
+	ADCON0 = 0x03; //Selects ADChannel "i" and turns on ADC
+	//ADCON0 = (i << 4) + 3;
 	ADCON0bits.GO = 1; // Start conversion
 	while(ADCON0bits.DONE==1); // Wait for completion
 	ADCON0bits.ADON = 0;  // Disable A/D converter
@@ -47,5 +45,11 @@ void Initialize_ADC(void)	// configure A/D convertor
 {
 	ADCON0=0x03; 
     ADCON1=0x1D; 
-    ADCON2=0x29; 
+    ADCON2=0x29;
+	/*Configure ANSEL pins*/
+	ANSELA = 0b00101111;
+	ANSELB = 0b00111110;
+	ANSELC = 0b11011100;
+	ANSELD = 0b00000010;
+	ANSELE = 0b00000111;
 }
